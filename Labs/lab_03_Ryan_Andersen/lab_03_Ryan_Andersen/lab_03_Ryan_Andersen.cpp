25444bb55df8bdefd3bf6304313e1afe814f5cb2
Ryan Andersen <ryan.andersen117@gmail.com>
2017-10-04 14:13:50 -0600

    started lab 3

---
+++ Labs/lab_03_Ryan_Andersen/lab_03_Ryan_Andersen/lab_03_Ryan_Andersen.cpp
@@ -0,0 +1,30 @@
+// Author: Ryan Andersen
+// Assignment: lab 03
+// Instructor: Blair
+// Class: CS 1410
+// Date Written: 10/4/17
+// Description: a console program that uses recursion
+
+//I declare that the following source code was written solely by me.
+//I understand that copying any source code, in whole or in part, 
+// constitutes cheating, and that I will receive a zero on this project
+// if I am found in violation of this policy.
+
+#include "stdafx.h"
+
+
+int main()
+{
+
+	system("pause");
+    return 0;
+}
+
+int amountA(string s) {
+	int count = 0;
+
+	if (s.length() == 0) {
+		return count;
+	}
+}
+
---
+++ Labs/lab_03_Ryan_Andersen/lab_03_Ryan_Andersen/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int amountA(string s);
