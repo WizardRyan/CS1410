d018182b6b8d344d91c64340d825d2c8a9db0e13
Ryan Andersen <ryan.andersen117@gmail.com>
2017-10-09 07:55:48 -0600

    started buildout of project 3

---
+++ Projects/project_3_JRA_V1.0/project_3_JRA_V1.0/project_3_JRA_V1.0.cpp
@@ -0,0 +1,31 @@
+// project_3_JRA_V1.0.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int nums[] = { 1, 2, 3, 4, 5 };
+	int numelems = 5;
+
+	mySort(nums, numelems);
+
+	cout << endl << endl;
+	system("pause");
+    return 0;
+}
+
+void mySort(int nums[], int numelems) {
+	if (numelems == 0) {
+		return;
+	}
+
+
+}
+
+
+int sortHelp(int nums[], int index, int size) {
+
+}
+
