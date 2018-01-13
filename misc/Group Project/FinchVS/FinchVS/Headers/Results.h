cd5109bea65c0c6093d369ecde2ea75a5be7572b
Ryan Andersen <ryan.andersen117@gmail.com>
2018-01-13 14:30:00 -0700

    started chapter 8

---
+++ misc/Group Project/FinchVS/FinchVS/Headers/Results.h
@@ -0,0 +1,17 @@
+#pragma once
+class Results
+{
+private:
+
+public:
+	Results();
+	~Results();
+
+	//double temp;
+	//double RLight;
+	//double LLight;
+	//double x;
+	//double y;
+	//double z;
+};
+
