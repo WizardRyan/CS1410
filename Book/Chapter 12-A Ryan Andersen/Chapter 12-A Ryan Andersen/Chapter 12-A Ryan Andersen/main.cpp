e5bc169d5b924d29662d53eae7cab08f470438df
WizardRyan <ryan.andersen117@gmail.com>
2018-02-17 14:08:15 -0700

    finished chapter 12-A

---
+++ Book/Chapter 12-A Ryan Andersen/Chapter 12-A Ryan Andersen/Chapter 12-A Ryan Andersen/main.cpp
@@ -0,0 +1,31 @@
+// Author: Ryan Andersen
+// Assignment: Chapter 12-A
+// Instructor: Blair
+// Class: CS 1410
+// Date Written: 1/13/18
+// Description: a program for practicing Lists
+
+
+//I declare that the following source code was written solely by me.
+//I understand that copying any source code, in whole or in part, 
+// constitutes cheating, and that I will receive a zero on this project
+// if I am found in violation of this policy.
+#include "Header.h"
+
+int main() {
+
+	List fruit;
+	fruit.push_back("Orange");
+	fruit.push_back("Apple");
+	fruit.push_back("Banana");
+
+	cout << fruit.get_size() << endl;
+
+	fruit.erase(fruit.begin());
+
+	cout << fruit.get_size();
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
