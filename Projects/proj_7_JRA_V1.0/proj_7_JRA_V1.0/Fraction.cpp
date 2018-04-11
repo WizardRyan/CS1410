22e182544f352abfdbfd9523225986b65b37e3c3
WizardRyan <ryan.andersen117@gmail.com>
2018-04-10 22:28:55 -0600

    initial buildout, still needs testing and a driver

--- Projects/proj_7_JRA_V1.0/proj_7_JRA_V1.0/Fraction.cpp
+++ Projects/proj_7_JRA_V1.0/proj_7_JRA_V1.0/Fraction.cpp
@@ -1,23 +1,141 @@
 #include "Fraction.h"
 
 
+int getGcf(int n1, int n2) {
+	// derived from Euclid's algorithm https://en.wikipedia.org/wiki/Euclidean_algorithm
+	int tmp;
+	n1 = abs(n1);
+	n2 = abs(n2);
+	while (n1 > 0) {
+		tmp = n1;
+		n1 = n2 % n1;
+		n2 = tmp;
+	}
+	return n2;
+}
+
+int getLcm(int a, int b) {
+	return (a * b) / getGcf(a, b);
+}
+
+void equalizeDenominators(Fraction& a, Fraction& b) {
+	if (a.denominator != b.denominator) {
+		int aTemp = a.denominator;
+		a.denominator *= b.denominator;
+		a.numerator *= b.denominator;
+		b.denominator *= aTemp;
+		b.numerator *= aTemp;
+	}
+}
 
 Fraction::Fraction()
 {
 }
 
-
 Fraction::Fraction(int numerator, int denominator) : numerator{ numerator }
 {
-	denominator == 0 ? throw runtime_error("Cannot initialize denomiator as zero!") : this->denominator = denominator;
+	denominator == 0 ? throw runtime_error("Cannot initialize denominator as zero!") : this->denominator = denominator;
+	this->optimize();
+}
+
+Fraction::Fraction(int numerator): numerator{numerator}
+{
+	this->optimize();
 }
 
 Fraction::~Fraction()
 {
+	this->optimize();
 }
 
-Fraction Fraction::reduce(Fraction frac)
+void Fraction::reduce()
 {
+	int gcf = getGcf(this->numerator, this->denominator);
+	this->numerator /= gcf;
+	this->denominator /= gcf;
+}
 
-	return Fraction();
+void Fraction::noNeg()
+{
+	if (this->denominator != abs(this->denominator)) {
+		this->denominator *= -1;
+		this->numerator *= -1;
+	}
+}
+
+void Fraction::optimize()
+{
+	this->reduce();
+	this->noNeg();
+}
+
+Fraction & Fraction::operator++()
+{
+	this->numerator += this->denominator;
+	return *this;
+}
+
+Fraction & Fraction::operator--()
+{
+	this->numerator -= this->denominator;
+	return *this;
+}
+
+Fraction Fraction::operator++(int)
+{
+	Fraction temp = *this;
+	++(*this);
+	return temp;
+}
+
+Fraction Fraction::operator--(int)
+{
+	Fraction temp = *this;
+	--(*this);
+	return temp;
+}
+
+ostream& operator<<(ostream& out, const Fraction frac) {
+
+	out << frac.numerator << (frac.denominator == 1 ? "" : " / " + to_string(frac.denominator));
+}
+
+Fraction operator*(const Fraction&a, const Fraction& b) {
+	Fraction c(a.numerator * b.numerator, a.denominator * b.denominator);
+	c.optimize();
+	return c;
+}
+
+Fraction operator/(const Fraction&a, const Fraction& b) {
+	Fraction c(a.numerator * b.denominator, a.denominator * b.numerator);
+	c.optimize();
+	return c;
+}
+
+Fraction operator+(const Fraction& a,  const Fraction& b) {
+	Fraction aa = a;
+	Fraction bb = b;
+
+	equalizeDenominators(aa, bb);
+	Fraction c(aa.numerator + bb.numerator, aa.denominator + bb.denominator);
+	c.optimize();
+	return c;
+}
+
+Fraction operator-(const Fraction& a, const Fraction& b) {
+	Fraction aa = a;
+	Fraction bb = b;
+
+	equalizeDenominators(aa, bb);
+	Fraction c(aa.numerator - bb.numerator, aa.denominator - bb.denominator);
+	c.optimize();
+	return c;
+}
+
+bool operator==(const Fraction&a, const Fraction& b) {
+	Fraction aa = a;
+	Fraction bb = b;
+
+	equalizeDenominators(aa, bb);
+	return aa.numerator == bb.numerator;
 }
--- Projects/proj_7_JRA_V1.0/proj_7_JRA_V1.0/Fraction.h
+++ Projects/proj_7_JRA_V1.0/proj_7_JRA_V1.0/Fraction.h
@@ -8,14 +8,62 @@ class Fraction
 {
 public:
 	Fraction(int numerator, int denominator);
+	Fraction(int numerator);
 	Fraction();
 
 	~Fraction();
-	
-	static Fraction reduce(Fraction frac);
+
+	//the optimize function, calls noNeg and reduce
+	//no params
+	//no return
+	void optimize();
+
+	//this overloads the << operator, to be able to output a fraction
+	//accepts out, the stream to use, and frac, the fraction to output
+	//returns a stream
+	friend ostream& operator<<(ostream& out, const Fraction frac);
+
+	//each one of these functions overloads the given binary operator, allowing the correct operation to be performed
+	//accepts a and b, the fractions to be used
+	//returns a fraction or a bool, according to the appropriate operator
+	friend Fraction operator*(const Fraction&a, const Fraction& b);
+	friend Fraction operator/(const Fraction&a, const Fraction& b);
+	friend Fraction operator+(const Fraction& a, const Fraction& b);
+	friend Fraction operator-(const Fraction& a, const Fraction& b);
+	friend bool operator==(const Fraction&a, const Fraction& b);
+
+	//the equalizeDenominators function, makes demonitors of two fractions the same
+	//no accepts a and b, the fractions to be modified
+	//no return
+	friend void equalizeDenominators(Fraction& a, Fraction& b);
+
+
+	//these overlaod the post and pre increment operators, each of them add or subract one to the fraction
+	//no parameters
+	//no returns
+	Fraction& operator++();
+	Fraction& operator--();
+
+	Fraction operator++(int);
+	Fraction operator--(int);
+
+
 
 private:
+
 	int numerator = 0;
 	int denominator = 1;
+	//the reduce function, reduces a fraction to it's lowest terms
+	//accepts no params
+	//no return
+	void reduce();
+
+	//the noNeg function, makes sure the denominator isn't negative, and if it is, flip it
+	//no params
+	//no return
+	void noNeg();
+
+
+
 };
 
